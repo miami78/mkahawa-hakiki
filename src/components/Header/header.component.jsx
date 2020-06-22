729800929ce8a149c35516b5d75b58442b5f4d40
miami78 <miamilarry78@gmail.com>
2020-06-22 13:27:46 +0300

    add geolocation placeholder

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -4,14 +4,13 @@ import {ReactComponent as ReactLogo} from '../../assets/location-pin.svg';
 
 const Header = () => {
     return (
-      <div className="header">
-        <div className="logo-container">
-            <ReactLogo />
+        <div className="header">
+            <div className="logo-container"><ReactLogo /></div>
+            <div className="geo-info">Nairobi,Kenya</div>
+            MKAHAWA HAKIKI
+            <button className="custom-button">Add Restaurant</button>
         </div>
-        MKAHAWA HAKIKI
-        <button className="custom-button">Add Restaurant</button>
-      </div>
     );
-  }
+}
   
   export default Header;
