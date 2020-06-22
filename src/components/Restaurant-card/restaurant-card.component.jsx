aa1ccbe07b94c9c7f2600cd0fef0d085543a3306
miami78 <miamilarry78@gmail.com>
2020-06-22 16:15:34 +0300

    add overflow scroll

--- src/components/Restaurant-card/restaurant-card.component.jsx
+++ src/components/Restaurant-card/restaurant-card.component.jsx
@@ -38,6 +38,36 @@ const RestaurantCard = () => {
                     Closing soon: 12:00 · 0727 628707</p>
                 </div>
             </div>
+            <div className="section-divider-bottom-line"></div>
+            <div className= "section">
+                <div className="section-header">
+                    <h2>Kilimanjaro Food Court</h2>
+                </div>
+                <div className="section-details">
+                    <p>$Restaurant · Teachers Service Commission, Woodley/Kenyatta Golf Course Kilimanjaro
+                    Closing soon: 12:00 · 0727 628707</p>
+                </div>
+            </div>
+            <div className="section-divider-bottom-line"></div>
+            <div className= "section">
+                <div className="section-header">
+                    <h2>Kilimanjaro Food Court</h2>
+                </div>
+                <div className="section-details">
+                    <p>$Restaurant · Teachers Service Commission, Woodley/Kenyatta Golf Course Kilimanjaro
+                    Closing soon: 12:00 · 0727 628707</p>
+                </div>
+            </div>
+            <div className="section-divider-bottom-line"></div>
+            <div className= "section">
+                <div className="section-header">
+                    <h2>Kilimanjaro Food Court</h2>
+                </div>
+                <div className="section-details">
+                    <p>$Restaurant · Teachers Service Commission, Woodley/Kenyatta Golf Course Kilimanjaro
+                    Closing soon: 12:00 · 0727 628707</p>
+                </div>
+            </div>
             <div className= "section">
                 <div className="section-header">
                     <h2>Kilimanjaro Food Court</h2>
