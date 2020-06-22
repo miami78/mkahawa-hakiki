4c9a065661d32e487184b3959332a7312d580a60
miami78 <miamilarry78@gmail.com>
2020-06-22 12:04:07 +0300

    add header component

---
+++ src/components/Header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.styles.scss'
+
+const Header = () => {
+    return (
+      <div className="header">
+        THIS IS THE HEADER
+      </div>
+    );
+  }
+  
+  export default Header;
