7cd0557c3499630938164f2688df1a3825f46ded
miami78 <miamilarry78@gmail.com>
2020-06-22 18:17:57 +0300

    add geolocation function

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import './header.styles.scss';
 import {ReactComponent as ReactLogo} from '../../assets/location-pin.svg';
 
-const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container"><ReactLogo /></div>
-            <div className="geo-info">Nairobi,Kenya</div>
-            <h1>MKAHAWA HAKIKI</h1>
-            <button className="custom-button">Add Restaurant</button>
-        </div>
-    );
+class Header extends React.Component {
+    state = {
+        latitude: 0,
+        longitude: 0,
+        city: null,
+        country: null
+    }
+
+    componentDidMount = () => {
+        // Get position of user
+        
+    }
+    render() {
+        return (
+            <div className="header">
+                <div className="logo-container"><ReactLogo /></div>
+                <div className="geo-info">{this.state.city}{this.state.country}Nairobi,Kenya</div>
+                <h1>MKAHAWA HAKIKI</h1>
+                <button className="custom-button">Add Restaurant</button>
+            </div>
+        );
+    } 
 }
   
   export default Header;
