400c3468f27e8bc2ef9e5d04a96cd613e8aad44e
miami78 <miamilarry78@gmail.com>
2020-07-05 19:33:38 +0300

    add use state to render form

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,29 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './header.styles.scss';
+import RestaurantForm from '../Form/form.component';
 import {ReactComponent as ReactLogo} from '../../assets/location-pin.svg';
 
-class Header extends React.Component {
-    state = {
-        latitude: 0,
-        longitude: 0,
-        city: null,
-        country: null
-    }
-
-    componentDidMount = () => {
-        // Get position of user
-        
-    }
-    render() {
-        return (
-            <div className="header">
-                <div className="logo-container"><ReactLogo /></div>
-                <div className="geo-info">{this.state.city}{this.state.country}Nairobi,Kenya</div>
-                <h1>MKAHAWA HAKIKI</h1>
-                <button className="custom-button">Add Restaurant</button>
-            </div>
-        );
-    } 
+const Header= ()=> {
+    const [showForm, setshowForm] = useState(false);
+    return (
+        <div className="header">
+            <div className="logo-container"><ReactLogo /></div>
+            
+            <h1>MKAHAWA HAKIKI</h1>
+            <button onClick= {()=> setshowForm(!showForm)} className="custom-button">Add Restaurant</button>
+            {showForm && <div><RestaurantForm /></div>}
+        </div>
+    );
+    
 }
   
   export default Header;
