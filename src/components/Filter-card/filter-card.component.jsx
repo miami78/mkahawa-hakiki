d38f696224f23b8431415a46507f55710088ac22
miami78 <miamilarry78@gmail.com>
2020-07-06 19:09:49 +0300

    refractor filter-card

---
+++ src/components/Filter-card/filter-card.component.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Rate } from 'antd';
+import Restaurants from '../../data/api.json';
+
+import './filter-card.styles.scss';
+
+class FilterCard  extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      minRating: 1,
+    }
+  }
+  
+  //passing a function as a ref
+  handleChange(rating) {
+    this.setState({minRating:rating})
+    console.log(rating)
+    //props.parentFunction
+  }
+  
+  render() {
+    return (
+      <div className="review-card">
+          <h3>Filter by rating :</h3>
+          <Rate 
+            allowHalf 
+            onChange={(rating)=> this.handleChange(rating)}
+            value={this.state.minRating}
+          />
+      </div>
+    );
+  }
+}
+
+export default FilterCard;
