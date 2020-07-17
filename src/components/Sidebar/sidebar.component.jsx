dc31c4dcdf1b1bdfcecb64bbaf4d6448aa8415d5
miami78 <miamilarry78@gmail.com>
2020-07-17 18:23:39 +0300

    fix rating bug

--- src/components/Sidebar/sidebar.component.jsx
+++ src/components/Sidebar/sidebar.component.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import FilterCard from '../Filter-card/filter-card.component';
-import RestaurantReviewCard from '../Restaurant-review-card/restaurant-review-card.component';
+import React from "react";
+import FilterCard from "../Filter-card/filter-card.component";
+import RestaurantReviewCard from "../Restaurant-review-card/restaurant-review-card.component";
 
-import './sidebar.styles.scss';
+import "./sidebar.styles.scss";
 
 //pass in the name of a new function as props to the review card
 //React pass function as props
 //set state to hold rating
 class SideBar extends React.Component {
-    // Set up state in sidebar component, to hold the filtered rating.
-    constructor(props){
-        super(props);
-        this.state = {
-            filteredRating:[],
-        }
-    }
-// Make a function in sidebar that sets the state with a rating that is passed in.
-    handleFilterChange(rating) {
-        this.setState({filteredRating:rating})
-    }
-    render(){
-        return (
-            <div className="restaurant-card">
-                <FilterCard handleFilterChange = {this.handleFilterChange}/>
-                <RestaurantReviewCard rating={this.state.filteredRating}/>
-            </div>
-        );
-    }  
+  // Set up state in sidebar component, to hold the filtered rating.
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredRating: []
+    };
+  }
+  // Make a function in sidebar that sets the state with a rating that is passed in.
+  handleFilterChange=(rating)=> {
+    this.setState({ filteredRating: rating });
+  }
+  render() {
+    console.log("OLAAAA", this.state);
+    return (
+      <div className="restaurant-card">
+        <FilterCard handleFilterChange={this.handleFilterChange} />
+        <RestaurantReviewCard rating={this.state.filteredRating} />
+      </div>
+    );
+  }
 }
 
 export default SideBar;
-
