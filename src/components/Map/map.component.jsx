babac966ee5daa3c9aec5f9ed8c55438b8cc60fe
miami78 <miamilarry78@gmail.com>
2020-07-25 13:32:54 +0300

    fixed geolocation error

--- src/components/Map/map.component.jsx
+++ src/components/Map/map.component.jsx
@@ -28,24 +28,31 @@ export class MapContainer extends React.Component {
   }
   //Initialize google maps
   initMap() {
+    
     // Checks for geolocation
     if(navigator.geolocation){
+      
       // Get position from user and assign it to state
       navigator.geolocation.getCurrentPosition(position=>{
-        this.setState({userPosition:{
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }})
+        this.setState({
+          userPosition:{
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }});
         console.log(this.state.userPosition)
-      });
-      // Create new instance of google map
+      
+        // Create new instance of google map
       let map;
-      map = new window.google.maps.Map(this.refs.map,{
+      map = new window.google.maps.Map(document.getElementById('map'),{
         center: this.state.userPosition,
-        zoom:15,
-        styles:mapStyles
+        zoom: 15,
+        styles: mapStyles
       })
+      console.log(this.state.userPosition)
+
+      // event listener for mapbounds
       this.mapBoundsListener(map)
+      
       //Create instance of a user marker on the map
       let userMarker = new window.google.maps.Marker({
         position: this.state.userPosition,
@@ -53,6 +60,8 @@ export class MapContainer extends React.Component {
         icon: userIcon
       })
       this.setState({map:map})
+      });
+      
     }else{
       alert("Geolocation not available")
     }
@@ -95,8 +104,7 @@ export class MapContainer extends React.Component {
   render() {
     return (
       <div 
-        id="google-map"
-        ref="map"
+        id="map"
         style= {mapStyles}
       />
       
