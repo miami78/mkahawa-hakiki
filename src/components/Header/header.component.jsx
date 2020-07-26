d816ccebe5d96ec14380f3232ca0f8e9d169cfdf
miami78 <miamilarry78@gmail.com>
2020-07-26 14:45:16 +0300

    changed logo

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
 import './header.styles.scss';
 import RestaurantForm from '../Form/form.component';
-import {ReactComponent as ReactLogo} from '../../assets/location-pin.svg';
+
 
 //Pass a function ref as props to trigger a function to the parent component
 const Header= ()=> {
     const [showForm, setshowForm] = useState(false);
     return (
         <div className="header">
-            <div className="logo-container"><ReactLogo /></div>
-            <div className="heading"><h1>MKAHAWA HAKIKI</h1></div>
+            <div className="logo-container"><h1>MH</h1></div>
+            
             <span onClick= {()=> setshowForm(!showForm)} className="custom-button">Add Restaurant</span>
             {showForm && <div><RestaurantForm /></div>}
         </div>
