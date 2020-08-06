42b87353c09a9662805f9cc7ea0fa093695c2b56
miami78 <miamilarry78@gmail.com>
2020-08-06 19:56:51 +0300

    add getDetails function

--- src/components/RestaurantCard/RestaurantCard.component.jsx
+++ src/components/RestaurantCard/RestaurantCard.component.jsx
@@ -13,24 +13,57 @@ class RestaurantCard extends React.Component {
         console.log(this.props)
     }
     handleClick=()=> {
-        this.setState({
-          showMore: true,
-        })
+        
+        //check if show more is false if it is then set it to true {
+            //check if isGoogle is true, 
+            // if so then do the getDetailsSearch
+            //store getDetails Review array in state
+            //else do nothing
+        //}
+        //else set show more to false
+        if(this.state.showMore === false){
+            //set showMore to true
+            this.setState({
+                showMore: true,
+              })
+            if(this.props.isGoogle === true){
+                console.log("google is true")
+                let request ={
+                    placeId: this.props.id,
+                    fields: ['reviews ', 'rating','icon','opening_hours']
+                }
+                let service = new window.google.maps.places.PlacesService(this.props.mapObject);
+                service.getDetails(request, getRestDetails);
+
+                function getRestDetails(result, status) {
+                    let reviewArray;
+                    if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                        console.log("status ok")
+                      result.push(reviewArray)
+                    }
+                  }
+            }
+            
+        }else{
+            this.setState({
+                showMore: false,
+              })
+              console.log("google is false")
+        }
         console.log("show more clicked")
     }
-      
+    
     render(){
-        const{ rating, restaurant, showMore } = this.props;
-        const numberOfRatings = showMore ? restaurant.length : 1
+        const numberOfRatings = this.state.showMore ? this.props.rating.length : 1
         return (
             <div className="section">
                 <div className="section-header">
-                    <h3>{restaurant.restaurantName}</h3>
+                    <h3>{this.props.restaurantName}</h3>
                 </div>
-                {rating.slice(0, numberOfRatings).map((rating, j) => ( 
+                {this.props.rating.slice(0, numberOfRatings).map((rating, j) => ( 
                     <div className="section-details">
                         <span><Rate disabled defaultValue={rating.stars} /></span>
-                        <p>{rating.comment}</p>
+                        <p>{this.props.rating.comment}</p>
                     </div>
                 ))}   
                 <div className="right-arrow">
