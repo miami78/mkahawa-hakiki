12704f5f6e4f11f2c64e81b83c652ae3a87427e8
miami78 <miamilarry78@gmail.com>
2020-08-07 22:37:03 +0300

    add get details service

--- src/components/RestaurantCard/RestaurantCard.component.jsx
+++ src/components/RestaurantCard/RestaurantCard.component.jsx
@@ -9,11 +9,10 @@ class RestaurantCard extends React.Component {
         super(props);
         this.state = {
             showMore: false,
+            restaurant:[]
         }
-        console.log(this.props)
     }
     handleClick=()=> {
-        
         //check if show more is false if it is then set it to true {
             //check if isGoogle is true, 
             // if so then do the getDetailsSearch
@@ -27,45 +26,40 @@ class RestaurantCard extends React.Component {
                 showMore: true,
               })
             if(this.props.isGoogle === true){
-                console.log("google is true")
                 let request ={
                     placeId: this.props.id,
-                    fields: ['reviews ', 'rating','icon','opening_hours']
+                    fields: ['name','rating','business_status','opening_hours','reviews']
                 }
                 let service = new window.google.maps.places.PlacesService(this.props.mapObject);
-                service.getDetails(request, getRestDetails);
-
-                function getRestDetails(result, status) {
-                    let reviewArray;
+                const getRestDetails=(place, status)=> {
                     if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-                        console.log("status ok")
-                      result.push(reviewArray)
+                        console.log("places service OK")
+                        console.log(place)
+                        this.setState({ restaurant: place });
+                        return {
+                            name: place.name,
+                            rating: place.rating,
+                            reviews: place.review
+                        }
                     }
-                  }
+                }
+                service.getDetails(request, getRestDetails);
             }
             
         }else{
             this.setState({
                 showMore: false,
               })
-              console.log("google is false")
         }
-        console.log("show more clicked")
     }
     
     render(){
-        const numberOfRatings = this.state.showMore ? this.props.rating.length : 1
         return (
             <div className="section">
                 <div className="section-header">
                     <h3>{this.props.restaurantName}</h3>
                 </div>
-                {this.props.rating.slice(0, numberOfRatings).map((rating, j) => ( 
-                    <div className="section-details">
-                        <span><Rate disabled defaultValue={rating.stars} /></span>
-                        <p>{this.props.rating.comment}</p>
-                    </div>
-                ))}   
+                
                 <div className="right-arrow">
                     <button className="right" onClick={()=>this.handleClick()}><i className="fas fa-angle-down"></i></button>
                 </div>
