65c5a710ede074d6b8a5ddcddb84a684a0609552
miami78 <miamilarry78@gmail.com>
2020-09-05 18:49:05 +0300

    remove add restaurant button

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,17 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './header.styles.scss';
-import RestaurantForm from '../Form/form.component';
 
 
 //Pass a function ref as props to trigger a function to the parent component
 const Header= ()=> {
-    const [showForm, setshowForm] = useState(false);
     return (
         <div className="header">
             <div className="logo-container"><h1>MH</h1></div>
-            
-            <span onClick= {()=> setshowForm(!showForm)} className="custom-button">Add Restaurant</span>
-            {showForm && <div><RestaurantForm /></div>}
         </div>
     );
     
