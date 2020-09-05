1390f3093989aac83f7b70eec3c67cd3bff18e36
miami78 <miamilarry78@gmail.com>
2020-09-05 18:51:30 +0300

    fix filter bug on google results

--- src/components/RestaurantList/RestaurantList.component.jsx
+++ src/components/RestaurantList/RestaurantList.component.jsx
@@ -14,16 +14,37 @@ class RestaurantList extends React.Component {
   }
   render() {
     
-    const filteredGRestaurants = this.props.gRestaurantData;;
-
+    const filteredGRestaurants = [];
+    for (let i = 0; i < this.props.gRestaurantData.length; i++) {
+      if (this.props.gRestaurantData[i].avgRating >= this.props.rating) {
+        filteredGRestaurants.push(this.props.gRestaurantData[i]);
+      }
+    }
     const filteredRestaurants = [];
     for (let i = 0; i < Restaurant.length; i++) {
       if (Restaurant[i].rating >= this.props.rating) {
         filteredRestaurants.push(Restaurant[i]);
       }
     }
+    const formRestaurants = [];
+    for (let i = 0; i < this.props.newRestaurants.length; i++) {
+      if (this.props.newRestaurants[i].rating >= this.props.rating) {
+        formRestaurants.push(this.props.newRestaurants[i]);
+      }
+    }
     return (
       <div className="restaurant-list">
+        {formRestaurants.map((restaurant, index)=>(
+          <RestaurantCard 
+          key={index}
+          isGoogle={false}
+          rating={restaurant.rating}
+          restaurantName={restaurant.restaurantName}
+          address={""}
+          photo={""}
+          reviews={[]}
+          />
+        ))}
         {filteredGRestaurants.map((restaurant, index)=>(
           <RestaurantCard 
           key={index}
@@ -36,14 +57,21 @@ class RestaurantList extends React.Component {
             photo={restaurant.photo}
             openNow={restaurant.openNow}
             totalRatings={restaurant.totalRatings}
+            lat={restaurant.lat}
+            lng={restaurant.lng}
           />
         ))}
         {filteredRestaurants.map((restaurant, index)=>(
           <RestaurantCard 
            key={index}
            isGoogle={false}
-           rating={restaurant.ratings}
+           rating={restaurant.rating}
            restaurantName={restaurant.restaurantName}
+           address={restaurant.address}
+           photo={restaurant.photo}
+           reviews={restaurant.reviews}
+           lat={restaurant.lat}
+           lng={restaurant.lng}
           />
         ))}
       </div>
